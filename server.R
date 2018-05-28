8bb63d2ecb9a3e05bb4eb735e40b41eb25ddb564
Jiage Qian <jiageq@uw.edu>
2018-05-28 14:26:31 -0700

    Question3 draft

--- server.R
+++ server.R
@@ -1,3 +1,30 @@
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
 my_server <- function(input, output) {
+  reactive_genre <- reactive({
+    input$genre
+  })
   
+  output$plot2 <- renderPlot({
+    filter_genre <- cleaned_reviews %>%
+      filter(genre == reactive_genre()) %>%
+      select(genre, score, pub_year) %>%
+      group_by(pub_year) %>%
+      summarise(average = mean(score))
+    
+    p2 <- ggplot(data = filter_genre) +
+      geom_line(mapping = aes(x = pub_year, y = average)) +
+      geom_point(mapping = aes(x = pub_year, y = average, color = reactive_genre())) +
+      labs(
+        title = "Genre Popularity Over Time", 
+        x = "Year (year)", # x-axis label 
+        y = "Score (out of 10)", # y-axis label 
+        color = "Genre"
+      )
+    p2
+  })
 }
+
+shinyServer(my_server)
--- ui.R
+++ ui.R
@@ -1,3 +1,22 @@
-my_ui <- fluidPage {
+library(shiny)
+library(dplyr)
+library(ggplot2)
+
+cleaned_reviews <- read.csv(file = "data/cleaned_reviews.csv", stringsAsFactors = FALSE)
+
+genre <- cleaned_reviews %>%
+  distinct(genre)
   
-}
+my_ui <- fluidPage (
+  sidebarLayout(
+    sidebarPanel(
+      selectInput('genre', label = "Choose a Music Genre:",
+                  genre)
+    ),
+    mainPanel(
+      plotOutput('plot2')
+    )
+  )
+)
+
+shinyUI(my_ui)
